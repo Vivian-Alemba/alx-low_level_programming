f8cb0614b40416381b1d4234cd1b6d7631ba7dc0
vivian-alemba <alembavivianauma@gmail.com>
2021-08-26 15:02:26 +0000

    created a c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,16 @@
+/*
+ *Using the puts function instead of printf
+ */
+
 #include<stdio.h>
+
+/**
+ *main - block code
+ *Return: 0.
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
