5c8bfc97a7e74b72e0dafa9ed55c2b55a3bf141a
vivian-alemba <alembavivianauma@gmail.com>
2021-08-26 15:15:07 +0000

    printing a word on a screen using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/*
+ *This is a prinf function used to print a string to the screen
+ */
+
+#include<stdio.h>
+
+/**
+ * main-block code
+ * Return:0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
