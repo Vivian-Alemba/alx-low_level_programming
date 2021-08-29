8e80f20d3fc2d62cfe46cc73ba5c65a8fe3d6dd6
vivian-alemba <alembavivianauma@gmail.com>
2021-08-29 12:15:35 +0000

    created more files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - block code
+ * Returns :0
+ */
+int main (void)
+{
+	char alph;
+	for (alph ='a'; alph <= 'z'; alph++ $$ alph ='A'; alph <='Z'; alph++)
+	{
+		putchar(alph)
+	}
+	return (0);	       
+
+}
