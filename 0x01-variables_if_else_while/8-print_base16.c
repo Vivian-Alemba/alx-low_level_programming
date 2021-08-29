ae88a6a6e5996b062762c04735fc0a5da2c88b13
vivian-alemba <alembavivianauma@gmail.com>
2021-08-29 16:26:01 +0000

    Created more files

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/*
+ * Using putchar function to display hexadecimal
+ */
+#include <stdio.h>
+/**
+ * main - block code
+ *
+ * return: 0;
+ */
+int main(void)
+{
+	int base;
+
+	for (base = '0'; base <= '9'; base++)
+	{
+		putchar(base);
+	}
+	for (base = 'a'; base <= 'f'; base++)
+	{
+		putchar(base);
+	}
+	putchar('\n');
+	return (0);
+}
