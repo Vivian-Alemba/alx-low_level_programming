154d29838e6d257ca4d84f9034f0f6b2980eb14d
vivian-alemba <alembavivianauma@gmail.com>
2021-09-01 11:46:43 +0000

    Added more files

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,16 @@
 #include "main.h"
 #include<stdio.h>
+
+/**
+ *print_alphabet - prints the alphabet, in lowercase.
+ *
+ */
 void print_alphabet(void)
 {
 	char *alph = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 27; i++)
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(alph[i]);
 	}
