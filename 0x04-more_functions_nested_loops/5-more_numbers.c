ab1d41f93c941f92816c3c29bb0c2aa8864f3cbb
vivian-alemba <alembavivianauma@gmail.com>
2021-09-03 18:57:18 +0000

    added files

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints 10 times numbers from 0-14
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	int c, n;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n / 10 != 0)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+	}
+
+}
