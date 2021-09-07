64c2b7e4117a893a9754fdc5958e76236be736ca
vivian-alemba <alembavivianauma@gmail.com>
2021-09-07 14:29:15 +0000

    Added more files

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function that prints a string
+ * @str: A pointer to the character
+ *
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
