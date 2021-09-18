86076fffd30f1b727f02ba42e9cef52800628e31
vivian-alemba <alembavivianauma@gmail.com>
2021-09-18 14:56:14 +0000

    Created a program that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: a pointer to the character destination
+ * @src: a pointer to the character source
+ * @n: number of bytes
+ *
+ * Return: nothing
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
