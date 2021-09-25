e0b23a7592c2dc29e321e7bffbedc1724e4a8d07
vivian-alemba <alembavivianauma@gmail.com>
2021-09-25 17:49:52 +0000

    Fixed the betty styling code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,7 +12,17 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest, src, sizeof(n));
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 
 
