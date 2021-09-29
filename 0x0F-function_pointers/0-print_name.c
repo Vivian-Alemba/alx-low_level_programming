267885ba328f5ee9a48baf5c4b6963e0e64d9a64
vivian-alemba <alembavivianauma@gmail.com>
2021-09-29 18:23:11 +0000

    Fixed the code

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,10 +5,9 @@
  * print_name - prints a name as is
  *@name: a pointer to the name of the person
  *@f: a pointer to the function
- *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 		{
-		f(name);
+		(*f)(name);
 		}
